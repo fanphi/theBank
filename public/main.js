9e2506812e4dd0882ddd40485839f3155a13c258
fanphi <fannyp90@hotmail.com>
2022-10-26 11:21:29 +0200

    update

--- public/main.js
+++ public/main.js
@@ -19,171 +19,156 @@ let registerSection = document.querySelector("#register");
 let loginSection = document.querySelector("#login");
 let loginContainer = document.querySelector("#login-div");
 
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-  
-    await fetch('/api/account/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: loginUser.value,
-        password: loginPassword.value
-      })
-    });
-    
-    console.log("log in");
-    location.reload();
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  await fetch("/api/account/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: loginUser.value,
+      password: loginPassword.value,
+    }),
   });
 
-  let random = Math.floor(Math.random()*10000);
+  console.log("log in");
+  location.reload();
+});
 
+let random = Math.floor(Math.random() * 10000);
 
-  registerForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const res = await fetch("/api/account/create", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: userInput.value,
-        password: passwordInput.value,
-        name: nameInput.value,
-        total: parseInt(totalInput.value),
-        accountnumber: random,
-        accounttype: accountInput.value
-      })
-    });
-    const data = res.json();
-  alert("The account has been registered!")
+registerForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const res = await fetch("/api/account/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: userInput.value,
+      password: passwordInput.value,
+      name: nameInput.value,
+      total: parseInt(totalInput.value),
+      accountnumber: random,
+      accounttype: accountInput.value,
+    }),
+  });
+  const data = res.json();
+  alert("The account has been registered!");
   registerForm.reset();
-  });
+});
 
-  const checkLoggedin = async () => { 
-    const res = await fetch('/api/account/loggedin');
-    const data = await res.json();
+const checkLoggedin = async () => {
+  const res = await fetch("/api/account/loggedin");
+  const data = await res.json();
   console.log(data.user);
-    if (data.user) {
+  if (data.user) {
     console.log("welcome" + " " + data.user.name);
-      depContainer.style.display = "flex";
-      accountContainer.style.display = "flex";
-      logoutForm.style.display = "flex";
-      loginContainer.style.display = "flex";
-      registerSection.style.display = "none";
-      loginSection.style.display = "none";
+    depContainer.style.display = "flex";
+    accountContainer.style.display = "flex";
+    logoutForm.style.display = "flex";
+    loginContainer.style.display = "flex";
+    registerSection.style.display = "none";
+    loginSection.style.display = "none";
 
-        let account = data.user
-      
-        let accountInfo = document.createElement("h2");
-        let accountName = document.createElement("h3");
-        let accountTotal = document.createElement("p");
-        let accountNum = document.createElement("p");
-        let accountType = document.createElement("p");
-        let accountUser = document.querySelector("h2");
-        
-        accountInfo.innerText = "Account Info"
-        accountUser.innerText = "Welcome " + account.user;
-        accountName.innerText = "Name: " + account.name;
-        accountType.innerText  = "Account type: " + account.accounttype;
-        accountTotal.innerText = "Total: " + account.total;
-        accountNum.innerText = "Account number: " + account.accountnumber;
-        
+    let account = data.user;
 
-        loginContainer.append(accountUser);
-        accountContainer.append(accountInfo, accountName, accountType, accountNum, accountTotal);
-    
-  
-    } else {
-      console.log("sign in" + data)
-    }
+    let accountInfo = document.createElement("h2");
+    let accountName = document.createElement("h3");
+    let accountTotal = document.createElement("p");
+    let accountNum = document.createElement("p");
+    let accountType = document.createElement("p");
+    let accountUser = document.querySelector("h2");
 
-    depositForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-  
-      
-        await fetch(`/api/account/update/${data.user._id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
+    accountInfo.innerText = "Account Info";
+    accountUser.innerText = "Welcome " + account.user;
+    accountName.innerText = "Name: " + account.name;
+    accountType.innerText = "Account type: " + account.accounttype;
+    accountTotal.innerText = "Total: " + account.total;
+    accountNum.innerText = "Account number: " + account.accountnumber;
 
-         user: data.user.user,
+    loginContainer.append(accountUser);
+    accountContainer.append(
+      accountInfo,
+      accountName,
+      accountType,
+      accountNum,
+      accountTotal
+    );
+  } else {
+    console.log("sign in" + data);
+  }
+
+  depositForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    await fetch(`/api/account/update/${data.user._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: data.user.user,
         password: data.user.password,
         name: data.user.name,
         total: parseInt(depositInput.value) + parseInt(data.user.total),
         accountnumber: data.user.accountnumber,
-        accounttype: data.user.accounttype
+        accounttype: data.user.accounttype,
+      }),
+    });
 
-          })
-        });
+    depositForm.reset();
+    location.reload();
+  });
 
-        depositForm.reset();
-       location.reload();
-
-        
-      });
-
-
-
-        withdrawForm.addEventListener('submit', async (e) => {
-          e.preventDefault();
-          if(data.user.total >= withdrawInput.value){
-         
-           
-          await fetch(`/api/account/update/${data.user._id}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-  
-           user: data.user.user,
+  withdrawForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    if (data.user.total >= withdrawInput.value) {
+      await fetch(`/api/account/update/${data.user._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: data.user.user,
           password: data.user.password,
           name: data.user.name,
           total: parseInt(data.user.total) - parseInt(withdrawInput.value),
           accountnumber: data.user.accountnumber,
-          accounttype: data.user.accounttype
-  
-            })
-          });
-  
-          withdrawForm.reset();
-         location.reload();
-         
-        }
-        else{
-          alert("You do not have enough money for the withdrawal. Please try another amount.");
-          location.reload()
-        }
-      
-        });
-        
-      
-        deleteForm.addEventListener('submit', async (e) => {
-          e.preventDefault();
-          
-            await fetch(`/api/account/delete/${data.user._id}`, {
-              method: 'DELETE'
-            });
-          
-          console.log("deleted" + data.user.name)
-          location.reload();
-          });
-        
- 
-  }
+          accounttype: data.user.accounttype,
+        }),
+      });
 
-  logoutForm.addEventListener('submit', async (e) => {
+      withdrawForm.reset();
+      location.reload();
+    } else {
+      alert(
+        "You do not have enough money for the withdrawal. Please try another amount."
+      );
+      location.reload();
+    }
+  });
+
+  deleteForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-  
-    await fetch('/api/account/logout', { method: 'POST' });
-    
+
+    await fetch(`/api/account/delete/${data.user._id}`, {
+      method: "DELETE",
+    });
+
+    console.log("deleted" + data.user.name);
     location.reload();
   });
-  
-  checkLoggedin();
+};
 
+logoutForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
+  await fetch("/api/account/logout", { method: "POST" });
+
+  location.reload();
+});
+
+checkLoggedin();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import session from 'express-session';
-import { MongoClient, ObjectId } from 'mongodb';
-import bcrypt from 'bcrypt';
+import express from "express";
+import session from "express-session";
+import { MongoClient, ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
 
 const port = 3000;
 const app = express();
@@ -9,81 +9,81 @@ const saltRounds = 10;
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const client = new MongoClient('mongodb://127.0.0.1:27017');
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 await client.connect();
-const db = client.db('bank');
-const accountCollection = db.collection('account');
+const db = client.db("bank");
+const accountCollection = db.collection("account");
 
-app.use(session({
-    resave: false, 
-    saveUninitialized: false, 
-    
-    secret: 'secret',
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+
+    secret: "secret",
     cookie: {
-      maxAge: 5 * 60 * 1000 
-    }
-  }));
+      maxAge: 5 * 60 * 1000,
+    },
+  })
+);
 
+app.post("/api/account/login", async (req, res) => {
+  const user = await accountCollection.findOne({ user: req.body.user });
+  const passMatches = await bcrypt.compare(req.body.password, user.password);
+  if (user && passMatches) {
+    req.session.user = user;
 
-  
-  
-  app.post('/api/account/login', async (req, res) => {
-    const user = await accountCollection.findOne({ user: req.body.user });
-    const passMatches = await bcrypt.compare(req.body.password, user.password);
-    if (user && passMatches) {
-      req.session.user = user;
-      
-      res.json({
-        user: user.user
-      });
-    } else { 
-      res.status(401).json({ error: 'Unauthorized' });
-    }
-  });
-
-  app.get('/api/account/loggedin', async (req, res) => {
-    if (req.session.user) {
-      const user = await accountCollection.findOne({ 
-        user: req.session.user.user });
-      req.session.user = user;
-
-      res.json({
-       
-        user: req.session.user
-      
-      });
-    } else { 
-      res.status(401).json({ error: 'Unauthorized' });
-    }
-  });
-
-  app.post('/api/account/logout', (req, res) => {
-    req.session.destroy(() => {
-      res.json({
-        loggedin: false
-      });
-    });
-  });
-
-  app.put('/api/account/update/:id', async (req, res) => {
-    let account = await accountCollection.findOne({ _id: ObjectId(req.params.id) });
-    account = {
-      ...account,
-      ...req.body
-    };
-    await accountCollection.updateOne({ _id: ObjectId(req.params.id) }, { $set: account });
     res.json({
-      success: true,
-      account
+      user: user.user,
+    });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+});
+
+app.get("/api/account/loggedin", async (req, res) => {
+  if (req.session.user) {
+    const user = await accountCollection.findOne({
+      user: req.session.user.user,
+    });
+    req.session.user = user;
+
+    res.json({
+      user: req.session.user,
+    });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+});
+
+app.post("/api/account/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.json({
+      loggedin: false,
     });
   });
- 
+});
 
+app.put("/api/account/update/:id", async (req, res) => {
+  let account = await accountCollection.findOne({
+    _id: ObjectId(req.params.id),
+  });
+  account = {
+    ...account,
+    ...req.body,
+  };
+  await accountCollection.updateOne(
+    { _id: ObjectId(req.params.id) },
+    { $set: account }
+  );
+  res.json({
+    success: true,
+    account,
+  });
+});
 
-
-app.post('/api/account/create', async (req, res) => {
+app.post("/api/account/create", async (req, res) => {
   const hash = await bcrypt.hash(req.body.password, saltRounds);
   const account = {
     user: req.body.user,
@@ -91,21 +91,18 @@ app.post('/api/account/create', async (req, res) => {
     name: req.body.name,
     total: req.body.total,
     accountnumber: req.body.accountnumber,
-    accounttype: req.body.accounttype
-  
+    accounttype: req.body.accounttype,
   };
   await accountCollection.insertOne(account);
   res.json({
     success: true,
-    account
+    account,
   });
 });
 
-
-  app.delete('/api/account/delete/:id', async (req, res) => {
-    await accountCollection.deleteOne({ _id: ObjectId(req.params.id) });
-    res.status(204).send();
-  });
-
+app.delete("/api/account/delete/:id", async (req, res) => {
+  await accountCollection.deleteOne({ _id: ObjectId(req.params.id) });
+  res.status(204).send();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
